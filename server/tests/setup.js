244deb28a621ec2bca8af4cc44bd2c3b856ceb05
Saeid Monfared <s_monfared1@yahoo.com>
2025-07-04 16:50:45 +0300

    fix DB name

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { startMongoMemoryServer, stopMongoMemoryServer } from './mongoServer';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.test'})
+dotenv.config({ path: '.env.test'});
 
 let mongo;
 
@@ -11,7 +11,8 @@ beforeAll( async () => {
 
     // const mongo = await startMongoMemoryServer();
     // const uri = mongo.getUri();
-
+    console.log(process.env.JWT_SECRET);
+    console.log(process.env.MONGO_URI);
     if (process.env.MONGO_URI) {
         uri = process.env.MONGO_URI;
         console.log("Using external Mongo URI:", uri);
@@ -26,6 +27,8 @@ beforeAll( async () => {
     if (!uri.startsWith("mongodb://127.0.0.1") && !uri.startsWith("mongodb://localhost")) {
         throw new Error(`Unsafe DB URI detected: ${uri}`);
         }
+
+    console.log(" before conn!!")
     const conn = await mongoose.connect(uri);
     console.log("connection:", conn.connection.readyState);
 
