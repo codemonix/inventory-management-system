a4e9ff7064c0b40411da1b996fd6dc73d248e4b5
Saeid Monfared <s_monfared1@yahoo.com>
2025-08-03 11:42:14 +0300

    fix: lazy load api config for testing and production

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 import { getToken } from "../utils/auth.js";
 import { logDebug, logInfo } from "../utils/logger.js";
-import config from "../config.js";
+import { getApiBaseUrl } from "../config.js";
+// import config from "../config.js";
 
 // const backendUrl = import.meta.env.VITE_API_BASE_URL;
 // logInfo("backendUrl:", backendUrl);
 // const API = `${backendUrl}` || "http://localhost:5000/api";
-const BACKEN_API_URL = config.API_URL || "http://localhost:5000/api";
-logDebug("config itself:", config)
-const api = axios.create({
-    baseURL: config.BACKEN_API_URL,
-});
+
+const api = axios.create();
 
 api.interceptors.request.use((config) => {
-    logInfo("actual URL:", BACKEN_API_URL);
+    const baseUrl = getApiBaseUrl();
+    config.baseURL = baseUrl;
     const token = getToken();
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
--- client/src/config.js
+++ client/src/config.js
@@ -1,23 +1,19 @@
-import api from "./api/api";
-import App from "./App";
 import { logInfo } from "./utils/logger";
 
+export function getApiBaseUrl() {
+    if (import.meta.env.DEV) {
+        return `${import.meta.env.VITE_API_BASE_URL}:${import.meta.env.VITE_API_PORT}`;
+    }
 
-let API_URL, API_PORT;
-logInfo("import meta dev", import.meta.env.DEV)
-if (import.meta.env.DEV ) {
-    API_URL = import.meta.env.VITE_API_BASE_URL;
-    API_PORT = import.meta.env.VITE_API_PORT;
-} else {
-    API_URL = window?.__IMS_CONFIG__?.API_URL;
-    API_PORT = window?.__IMS_CONFIG__?.API_PORT;
+    // For production/test
+    const url = typeof window !== "undefined" ? window.__IMS_CONFIG__?.API_URL : null;
+    const port = typeof window !== "undefined" ? window.__IMS_CONFIG__?.API_PORT : null;
+
+    if (!url) {
+        throw new Error("Missing API_URL configuration!");
+    }
+
+    return port ? `${url}:${port}` : url;
 }
 
-if (!API_URL) {
-    throw new Error("Missing API_URL configuration!")
-}
 
-const fullApiUrl = API_PORT ? `${API_URL}:${API_PORT}/api` : `${API_URL}/api`;
-logInfo("fullApiUrl:", fullApiUrl)
-
-export default { API_URL: fullApiUrl, };
