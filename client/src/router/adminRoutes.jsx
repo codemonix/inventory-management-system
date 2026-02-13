a343a2086cadf592e11495c6b5b06d9fad07925f
Saeid Monfared <s_monfared1@yahoo.com>
2026-02-13 12:18:40 +0300

    fix: SPA Deep Link

--- client/src/router/adminRoutes.jsx
+++ client/src/router/adminRoutes.jsx
@@ -5,14 +5,15 @@ import { Route } from 'react-router-dom';
 const ManageConsole = Lazy(() => import('../pages/admin/ManagePage.jsx'));
 const UsersPage = Lazy(() => import('../pages/admin/UsersPage.jsx'));
 const LogsPage = Lazy(() => import('../pages/admin/LogsPage.jsx'));
-const SettingsPage = Lazy(() => import('../pages/admin/SettingsPage.jsx'));
+// const SettingsPage = Lazy(() => import('../pages/admin/SettingsPage.jsx'));
+const SystemPage = Lazy(() => import('../pages/admin/SystemPage.jsx'))
 
 export const consoleRoutes = (
     <Route path='admin' element={<ManageConsole />} >
         <Route index element={<UsersPage />} />
         <Route path='users' element={<UsersPage />} />
         <Route path='logs' element={<LogsPage />} />
-        <Route path='settings' element={<SettingsPage />} />
+        <Route path='system' element={<SystemPage />} />
     </Route>
 );
 
